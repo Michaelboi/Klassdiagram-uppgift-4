380dadcf7f11902aa3b377e437896712f6b0b6fb
Michaelboi <micnwa050628@student.jenseneducation.se>
2022-11-08 11:24:50 +0100

    Add project files.

---
+++ Klassdiagram uppgift 4/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klassdiagram_uppgift_4
+{
+    public class Animal
+    {
+        private string name;
+        private int age;
+        public string getName()
+        {
+            return this.name;
+        }
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public class Dog : Animal
+        {
+            private int svanslängd;
+
+            public int getsvanslängd()
+            {
+                return this.svanslängd;
+            }
+            public void setsvanslängd(int svanslängd)
+            {
+                this.svanslängd = svanslängd;
+            }
+            public void Bark()
+            {
+                Console.WriteLine("woof");
+            }
+        }
+    }
+    
+}
---
+++ Klassdiagram uppgift 4/Program.cs
@@ -0,0 +1,14 @@
+﻿using Klassdiagram_uppgift_4;
+
+void Djur()
+{
+    // jag använder get och set funktioner här
+    Animal.Dog hund = new Animal.Dog();
+    hund.setName("Malkolm");
+    Console.WriteLine("namn: " + hund.getName());
+    hund.setsvanslängd(50);
+    Console.WriteLine("svanslängd: " + hund.getsvanslängd());
+    hund.Bark();
+    
+}
+Djur();
